f185d1a6d11bd4376ce9f6d99d1de276413e3cac
jhaip <jhaip@mit.edu>
2015-11-08 12:21:07 -0500

    first commit

---
+++ WebSocketClientTest2/WebSocketClientTest2.ino
@@ -0,0 +1,98 @@
+/*
+ * WebSocketClient.ino
+ *
+ *  Created on: 24.05.2015
+ *
+ */
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <WebSocketsClient.h>
+
+#include <Hash.h>
+
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+
+
+#define USE_SERIAL Serial
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght) {
+
+
+    switch(type) {
+        case WStype_DISCONNECTED:
+            USE_SERIAL.printf("[WSc] Disconnected!\n");
+            break;
+        case WStype_CONNECTED:
+            {
+                USE_SERIAL.printf("[WSc] Connected to url: %s\n",  payload);
+            }
+            break;
+        case WStype_TEXT:
+            USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+
+            // send data to back to Server
+            //webSocket.sendTXT(payload, lenght);
+            break;
+        case WStype_BIN:
+            USE_SERIAL.printf("[WSc] get binary lenght: %u\n", lenght);
+            hexdump(payload, lenght);
+
+            // echo data back to Server
+            //webSocket.sendBIN(payload, lenght);
+            break;
+    }
+
+}
+
+
+void setup() {
+    // USE_SERIAL.begin(921600);
+    USE_SERIAL.begin(115200);
+
+    //Serial.setDebugOutput(true);
+    USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+      for(uint8_t t = 4; t > 0; t--) {
+          USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+          USE_SERIAL.flush();
+          delay(1000);
+      }
+
+    WiFiMulti.addAP("STALY2.4", "1 am a tr33");
+
+    //WiFi.disconnect();
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    webSocket.begin("192.168.1.14", 81);
+    webSocket.onEvent(webSocketEvent);
+
+}
+
+int count = 0;
+
+void loop() {
+    webSocket.loop();
+    count += 1;
+    webSocket.sendTXT("Beginning");
+    //webSocket.loop();
+    delay(5);
+    webSocket.sendTXT("middle ");
+    webSocket.sendTXT("Beginning ");
+    //webSocket.loop();
+    delay(10);
+    if (!digitalRead(0)) {
+      int val = count % 2;
+      webSocket.sendTXT("val="+String(val)+" "+micros());
+    }
+}
---
+++ app.js
@@ -0,0 +1,166 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongo = require('mongodb');
+var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+var WebSocketServer = require('websocket').server;
+var db;
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+var url = 'mongodb://localhost:27017/testdb';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, _db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+  db = _db;
+  //_db.close();
+});
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+// parse application/json 
+app.use(bodyParser.json());
+app.use(function(req,res,next) {
+    req.db = db;
+    next();
+})
+
+app.post('/hello', function (req, res) {
+    console.log(req.body);
+    console.log(req.body.type);
+    res.send('Thanks.');
+});
+
+app.post('/projects/:project', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   //var db = req.db;
+   var data = req.body;
+   var project = req.params.project;
+   var type = data.type;
+   var label = data.label;
+   var value = data.value;
+   var d = new Date();
+   var timestamp = d.toISOString();
+
+   //var collection = db.collection('documents');
+
+   var payload = {
+        "type": type,
+        "label": label,
+        "value": value,
+        "timestamp": timestamp
+   };
+
+   //io.emit('hello', payload);
+   res.send('Thanks');
+   /*
+   collection.insert(payload, function(err, doc) {
+        if (err) {
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            io.emit('hello', payload);
+            res.send('Thanks');
+        }
+   });
+*/
+});
+
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+app.get('/projects/:project', function(req, res) {
+    //var db = req.db;
+    var data = req.body;
+    var project = req.params.project;
+    var collection = db.collection(project);
+    collection.find({}).toArray(function(err, docs) {
+        console.log("got it");
+        if (err) {
+            res.send("There was a problem getting the information from the database.");
+        }
+        else {
+            res.sendFile('index.html', { root: path.join(__dirname, './') });
+        }
+    });
+});
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   io.emit('hello', "Well hello");
+//   socket.on('chat message', function(msg){
+//     console.log('message: ' + msg);
+//   });
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+
+http.listen(81, function(){
+  console.log('listening on *:3000');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: http,
+    // You should not use autoAcceptConnections for production 
+    // applications, as it defeats all standard cross-origin protection 
+    // facilities built into the protocol and the browser.  You should 
+    // *always* verify the connection's origin and decide whether or not 
+    // to accept it. 
+    autoAcceptConnections: false
+});
+ 
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed. 
+  return true;
+}
+
+var connections = [];
+ 
+wsServer.on('request', function(request) {
+    
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin 
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('arduino', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connections.push(connection);
+    console.log("Number of connections: "+connections.length);
+    
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            //for (var i=0; i<connections.length; i+=1) {
+            //    connections[i].sendUTF(message.utf8Data);   
+            //}            
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+        for (var i=0; i<connections.length; i+=1) {
+            if (connections[i] == connection) {
+                connections.splice(i,1);
+            }
+        }
+        console.log("Number of connections: "+connections.length);
+    });
+    
+    connection.sendUTF("Hallo Client!");
+});
